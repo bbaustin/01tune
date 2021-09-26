6cc5af686e69c4b67d2b96f50302dc6099cfeb30
Ben <ben.austin@melodysquare.com>
2021-09-27 00:21:16 +0900

    Plays poly tones. Doesn't know chords yet

--- components/TonePlayer.tsx
+++ components/TonePlayer.tsx
@@ -8,19 +8,16 @@ interface tonePlayerProps {
 export default function TonePlayer(tones: any) {
   useEffect(() => {
     //create a synth and connect it to the main output (your speakers)
-    const synth = new Tone.Synth().toDestination();
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    const now = Tone.now();
     if (tones.tones) {
       console.log(tones);
       console.log(tones.tones[0]);
-
-      //synth.triggerAttackRelease("C4", "4n");   // <-- This works :)
-
-      // tones.forEach((tone: any) => {
-      //   synth.triggerAttackRelease(`${tone}4`, "4n");
-      // });
+      let timeIncrease = 0.5;
       for (var i = 0; i < 2; i++) {
         console.log(tones.tones[i]);
-        synth.triggerAttackRelease(`${tones.tones[i]}4`, "4n");
+        synth.triggerAttackRelease(`${tones.tones[i]}4`, timeIncrease);
+        timeIncrease += 0.5;
         // synth.triggerAttackRelease("C4", "4n");
       }
     }
